d676f9b03484cd2f01f68f44b87ce6281727247c
Luís Inácio <lrs.inacio@gmail.com>
2023-08-04 01:32:43 +0100

    Comment and post entities and database context

--- SM-POST/Post.Query/Post.Query.Api/Program.cs
+++ SM-POST/Post.Query/Post.Query.Api/Program.cs
@@ -1,15 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Post.Query.Infrastructure.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Action<DbContextOptionsBuilder> configureDbContext = 
+    o => o.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
+builder.Services.AddSingleton(new DatabaseContextFactory(configureDbContext));
+
+// create database and tables from code
+var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
+dataContext.Database.EnsureCreated();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
