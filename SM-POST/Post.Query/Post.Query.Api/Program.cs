a9ea9e5047295e63e75b9d96fc9661f2d02fe67b
Luís Inácio <lrs.inacio@gmail.com>
2023-08-04 20:45:26 +0100

    EventConsumer, eventHandler,BaseResponse and NewPost Controller

--- SM-POST/Post.Query/Post.Query.Api/Program.cs
+++ SM-POST/Post.Query/Post.Query.Api/Program.cs
@@ -1,5 +1,11 @@
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Handlers;
+using Post.Query.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +19,14 @@ builder.Services.AddOptions<DatabaseContext>().Configure((options) => options.Da
 
 // var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
 // dataContext.Database.EnsureCreated();
+
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
+builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
+builder.Services.AddScoped<IEventConsumer, EventConsumer>();
+
+builder.Services.AddHostedService<ConsumerHostedService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
