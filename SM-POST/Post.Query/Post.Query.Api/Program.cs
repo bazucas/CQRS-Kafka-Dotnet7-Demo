762094b2cb1bd11f87418b596c2125177c248bf3
Luís Inácio <lrs.inacio@gmail.com>
2023-08-05 15:40:39 +0100

    QueryDispatcher, QueryHandler and Find queries

--- SM-POST/Post.Query/Post.Query.Api/Program.cs
+++ SM-POST/Post.Query/Post.Query.Api/Program.cs
@@ -1,9 +1,13 @@
 using Confluent.Kafka;
 using CQRS.Core.Consumers;
+using CQRS.Core.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Api.Queries;
+using Post.Query.Domain.Entities;
 using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Dispatchers;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
 
@@ -22,11 +26,24 @@ builder.Services.AddOptions<DatabaseContext>().Configure((options) => options.Da
 
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IQueryHandler, QueryHandler>();
 builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
 builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
 builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 
 builder.Services.AddHostedService<ConsumerHostedService>();
+
+var dispatcher = new QueryDispatcher();
+builder.Services.AddOptions<IQueryHandler>().Configure((queryHandler) => 
+{
+    dispatcher.RegisterHandler<FindAllPostsQuery>(queryHandler.HandlerAsync);
+    dispatcher.RegisterHandler<FindPostByIdQuery>(queryHandler.HandlerAsync);
+    dispatcher.RegisterHandler<FindPostsByAuthorQuery>(queryHandler.HandlerAsync);
+    dispatcher.RegisterHandler<FindPostsWithCommentsQuery>(queryHandler.HandlerAsync);
+    dispatcher.RegisterHandler<FindPostsWithLikesQuery>(queryHandler.HandlerAsync);
+});
+builder.Services.AddSingleton<IQueryDispatcher<PostEntity>>(_ => dispatcher);
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
